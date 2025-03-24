98b0008097d7552e63a45d4fcfb001d4f07ff480
metttxd <metellimatteo1@gmail.com>
2025-03-24 17:54:30 +0100

    changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 // Configura CORS per accettare richieste da tutti i domini
 app.use(cors({
-  origin: "https://dontbej.com", // Accetta richieste da qualsiasi dominio
+  origin: "https://www.dontbej.com", // Accetta richieste da qualsiasi dominio
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
